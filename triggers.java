6c2c7a5ee1c24b981a0d5d0b2fdfad79549955f3
Carlos <89622246+RCBernal@users.noreply.github.com>
2025-10-01 17:16:45 -0600

    Create triggers.java

---
+++ triggers.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Vamos a obtener la variable para el entorno de Github
+        String nombre= Optional.ofNullable(System.getenv("USERNAME")).orElse("DefaultUser");
+        String language= Optional.ofNullable(System.getenv("LANGUAGE")).orElse("Default");
+        System.out.println("Hola ->" + nombre + " desde HitHub, tu lenguaje favorito es : " + language);
+
+        }
+    }
